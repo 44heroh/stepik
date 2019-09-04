0c0c1aedfe86a3e81de8f45cdd03b0a6aebb74d0
44heroh <44heroh@mail.ru>
2019-09-04 23:20:49 +0300

    stepik 2.2 Числа Фибоначчи Задача на программирование повышенной сложности: огромное число Фибоначчи по модулю

---
+++ src/ru/javastudy/HugeFibonachi.java
@@ -0,0 +1,42 @@
+package ru.javastudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HugeFibonachi {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        /*Scanner s = new Scanner(System.in);
+        Long a = s.nextLong();
+        Long b = s.nextLong();*/
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Long a = Long.parseLong(bufferedReader.readLine());
+        Long b = Long.parseLong(bufferedReader.readLine());
+        System.out.println(getHugeFibonacci(a, b));
+    }
+
+    private static long getHugeFibonacci(Long a, Long b) {
+        ArrayList<Long> arr = getPisanoPeriods(b);
+        int period = arr.size() - 2;
+        int res = (int)(a % period);
+        return res;
+    }
+
+    private static ArrayList<Long> getPisanoPeriods(Long b) {
+        ArrayList<Long> arr = new ArrayList();
+        arr.add((long)0);
+        arr.add((long)1);
+        for(int i = 2; i < b * 6; i++){
+            arr.add((arr.get(i - 1) + arr.get(i -2 )) % b);
+            if(arr.get(i) == 1 && arr.get(i - 1) == 0){
+                break;
+            }
+        }
+
+        return arr;
+    }
+}
